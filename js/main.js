f640e5b8da625e05c608bda1a3e46fa76b867849
AdamBabinicz <puaro@vp.pl>
2018-06-02 18:21:20 +0200

    koniec

--- js/main.js
+++ js/main.js
@@ -2,5 +2,60 @@
 "use strict";
 
 function validateForm() {
-	alert("it worked");
+	var status = true;
+	var x;
+	var i;
+
+	//reset the form
+	var requiredSection = ['fullName', 'phoneNumber', 'payment', 'ccNumber', 'vehicle'];
+	for (i = 0; i < requiredSections.length; i++;) {
+		document.getElementById(requiredSections[i]).className = "";
+	} // end loop
+	//test for full name
+	x = document.forms.myForm.name.value;
+	if (x === null || x === "") {
+		status = false;
+		document.getElementById('fullName').className = "error";
+	}
+	// check for the phone number
+	x = document.forms.myForm.phone.value;
+	x = x.replace(/-/g, '');
+	document.forms.myForm.phone.value = x;
+	if (x.length < 10 || x.length > 15) {
+		status = false;
+		document.getElementById('phoneNumber').className = "error";
+	}
+	//check the payment method
+	var foundCheckes = false;
+	x = document.getElementsByName('payMethod');
+
+	for (i = 0; i < x.length; i++) {
+		if (x[i].checked) {
+			foundChecked = true;
+			break;
+		} // end if
+	} // end for loop
+	console.log(foundChecked);
+	if (foundChecked === false) {
+		status = false;
+		document.getElementById('payment').className = "error";
+	}
+	// check for the cc number
+	x = document.forms.myForm.ccNumber.value;
+	x = x.replace(/ /g, '');
+
+	if (x.length < 14 || x.length > 19) {
+		status = false;
+		document.getElementById('ccNumber').className = "error";
+	}
+	// check the auto type
+	x = document.forms.myForm.vehicle.selectedIndex;
+	if (x === 0) {
+		status = false;
+		document.getElementById('ccNumber').className = "error";
+	}
+	if (status === false) {
+		document.getElementById('formProblems').className = "error";
+	}
+	return status;
 } //end of the form
